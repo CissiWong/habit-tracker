04fb91e1daf3e4f2b116422c95a666497008bdf1
linnsan2000 <31280120+linnsan2000@users.noreply.github.com>
2017-12-18 11:11:38 +0100

    Very basic structure

---
+++ code/src/components/views/frontpage.js
@@ -0,0 +1,15 @@
+import React from "react"
+
+class Frontpage extends React.Component {
+
+  render() {
+    return (
+      <div>
+        Find me in src/app.js!
+      </div>
+    )
+  }
+
+}
+
+export default Frontpage
