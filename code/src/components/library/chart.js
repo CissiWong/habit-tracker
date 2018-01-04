5bd01a09387744409976d5e3f1ee4642564ba3e4
Cecilia Wong <cissi.wong@gmail.com>
2018-01-04 15:46:24 +0100

    new pie chart

--- code/src/components/library/chart.js
+++ code/src/components/library/chart.js
@@ -1,16 +1,41 @@
 import React from "react"
-import { PieChart, Pie, Tooltip } from "recharts"
+import { PieChart, Pie, Cell } from "recharts"
+
+  const COLORS = ['#e53c53', '#85d171'];
+  const RADIAN = Math.PI / 180;
+
+
 
 export default class Chart extends React.Component {
 
-  render () {
-    let total = this.props.total
-    const data = [{name:"done", value: this.props.done / total, color: "green"}, {name:"notDone", value: this.props.notDone / total, color: "red"}]
+	  render() {
+      const total =  this.props.total
+      const data = [
+        { name: "done", value: this.props.done / total },
+        { name: "notDone", value: this.props.notDone / total }
+      ]
   	return (
-      <PieChart width={800} height={400}>
-        <Pie data={data} cx={500} cy={200} innerRadius={40} outerRadius={80} fill="#82ca9d"/>
-        <Tooltip />
+      <PieChart width={800} height={400} onMouseEnter={this.onPieEnter}>
+        <Pie
+          data={data}
+          cx={120}
+          cy={200}
+          innerRadius={60}
+          outerRadius={80}
+          fill="#8884d8"
+          paddingAngle={5} >
+          {
+            data.map((entry, index) => <Cell fill={COLORS[index % COLORS.length]} />)
+          }
+        </Pie>
       </PieChart>
-      );
-    }
+    )
   }
+}
+
+  //
+  // return (
+  //   <PieChart width={800} height={400}>
+  //     <Pie data={data} cx={500} cy={200} innerRadius={40} outerRadius={80} fill="#82ca9d"/>
+  //     <Tooltip />
+  //   </PieChart>
