de80cd3053d982f89d23dcf6e72d647c3d835a58
linnsan2000 <31280120+linnsan2000@users.noreply.github.com>
2018-01-04 11:19:43 +0100

    Knappen är tillbaka

--- code/src/components/library/input.js
+++ code/src/components/library/input.js
@@ -54,7 +54,7 @@ class Input extends React.Component {
         <form>
           <div className="container">
             <label htmlFor="activity">
-              <h1>Jag ska:</h1>
+              <h1>DIN AKTIVITET:</h1>
               <input
                 className="activity"
                 id="activity"
@@ -62,18 +62,18 @@ class Input extends React.Component {
                 type="text"
                 onChange={this.handleChange} />
             </label>
-            <h1>När då?</h1>
+            <h1>VILKEN DAG?</h1>
           </div>
-          <input type="radio" name="day" value="0" onChange={this.changeDay} />Måndag
-          <input type="radio" name="day" value="1" onChange={this.changeDay} />Tisdag
-          <input type="radio" name="day" value="2" onChange={this.changeDay} />Onsdag
-          <input type="radio" name="day" value="3" onChange={this.changeDay} />Torsdag
-          <input type="radio" name="day" value="4" onChange={this.changeDay} />Fredag
-          <input type="radio" name="day" value="5" onChange={this.changeDay} />Lördag
-          <input type="radio" name="day" value="6" onChange={this.changeDay} />Söndag
+          <input type="radio" name="day" value="0" onChange={this.changeDay} />MÅ
+          <input type="radio" name="day" value="1" onChange={this.changeDay} />TI
+          <input type="radio" name="day" value="2" onChange={this.changeDay} />ON
+          <input type="radio" name="day" value="3" onChange={this.changeDay} />TO
+          <input type="radio" name="day" value="4" onChange={this.changeDay} />FR
+          <input type="radio" name="day" value="5" onChange={this.changeDay} />LÖ
+          <input type="radio" name="day" value="6" onChange={this.changeDay} />SÖ
           <br />
           <div className="icon-container">
-            <h1>Välj en bild:</h1>
+            <h1>VÄLJ EN SYMBOL:</h1>
             <div className="icons">
               {
                 icons.map(symbol => (<chooseIcon
@@ -81,6 +81,7 @@ class Input extends React.Component {
                   setIcon={this.handleChooseIcon} />
                 ))}
             </div>
+            <button className="add-button" onClick={this.handleSubmit}>LÄGG TILL</button>
           </div>
         </form>
       </div>
